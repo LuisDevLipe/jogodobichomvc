89323520dd1031956f04a326430638ce7079f918
LuisDevLipe <luisfelipebsj@gmail.com>
2024-10-26 15:51:03 -0300

    controllers

--- core/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace Core\Controllers;
-
-    class HomeController
-    {
-        public function index() {
-            require_once __DIR__ . '/../views/home.php';
-        }
-    }
